dec51b1cec4555e502868d11fdd4e4355999dcdf
ylc395 <cyl@cyl.moe>
2022-10-14 12:00:02 +0800

    add watcher for electron

--- script/dev-electron.js
+++ script/dev-electron.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { createServer, build } = require('vite');
 const pluginVue = require('@vitejs/plugin-vue');
 const shell = require('shelljs');
+const chokidar = require('chokidar');
 const { default: tsconfigPaths } = require('vite-tsconfig-paths');
 
 const WEB_TSCONFIG = 'src/client/driver/web/tsconfig.json';
@@ -10,6 +11,18 @@ const ELECTRON_OUTPUT = 'dist/electron';
 const ELECTRON_TSCONFIG = 'src/server/driver/electron/tsconfig.json';
 const PATH_TSCONFIG = 'src/server/tsconfig.json';
 const ELECTRON_TS_FLAG = `--project ${ELECTRON_TSCONFIG} --outDir ${ELECTRON_OUTPUT}`;
+const ELECTRON_RELATED_DIRS = ['src/server', 'src/client/driver/electron', 'src/shared'];
+
+function callShell(viteUrl) {
+  shell.env['VITE_SERVER_ENTRY_URL'] = viteUrl;
+  shell.env['NODE_ENV'] = 'development';
+
+  shell.exec(`tsc ${ELECTRON_TS_FLAG}`);
+  shell.exec(`resolve-tspaths --project ${PATH_TSCONFIG} --out ${ELECTRON_OUTPUT}`);
+  let electronProcess = shell.exec(`electron ${ELECTRON_OUTPUT}/server/index.js`, { async: true });
+
+  return electronProcess;
+}
 
 (async () => {
   await build({
@@ -44,10 +57,11 @@ const ELECTRON_TS_FLAG = `--project ${ELECTRON_TSCONFIG} --outDir ${ELECTRON_OUT
   await server.listen();
   server.printUrls();
 
-  shell.env['VITE_SERVER_ENTRY_URL'] = server.resolvedUrls.local[0];
-  shell.env['NODE_ENV'] = 'development';
+  let shellProcess = callShell(server.resolvedUrls.local[0]);
 
-  shell.exec(`tsc ${ELECTRON_TS_FLAG}`);
-  shell.exec(`resolve-tspaths --project ${PATH_TSCONFIG} --out ${ELECTRON_OUTPUT}`);
-  shell.exec(`electron ${ELECTRON_OUTPUT}/server/index.js`, { async: true });
+  chokidar.watch(ELECTRON_RELATED_DIRS, { ignoreInitial: true }).on('all', (event, path) => {
+    console.log(path, event);
+    shellProcess.kill();
+    shellProcess = callShell(server.resolvedUrls.local[0]);
+  });
 })();
