9efdd494635f66ad02c6685cce9c750c06e6181a
ylc395 <cyl@cyl.moe>
2022-10-25 15:57:09 +0800

    move tsconfig.web.json

--- script/dev-electron.js
+++ script/dev-electron.js
@@ -5,11 +5,14 @@ const shell = require('shelljs');
 const chokidar = require('chokidar');
 const { default: tsconfigPaths } = require('vite-tsconfig-paths');
 const { replaceTscAliasPaths } = require('tsc-alias');
+const { parse } = require('tsconfck');
 
 const CLIENT_TSCONFIG = 'src/client/tsconfig.json';
 const ELECTRON_OUTPUT = 'dist/electron';
 
 async function buildPreload() {
+  // preload script must be processed by a bundler, since `require` doesn't work
+  // @see https://www.electronjs.org/docs/latest/tutorial/sandbox#preload-scripts
   await build({
     build: {
       minify: false,
@@ -32,8 +35,7 @@ async function buildPreload() {
 async function buildElectron(viteUrl) {
   const ELECTRON_SERVER_TSCONFIG = 'src/server/tsconfig.electron.json';
   const ELECTRON_CLIENT_TSCONFIG = 'src/client/tsconfig.electron.json';
-  // preload script must be processed by a bundler, since `require` doesn't work
-  // @see https://www.electronjs.org/docs/latest/tutorial/sandbox#preload-scripts
+
   shell.env['VITE_SERVER_ENTRY_URL'] = viteUrl;
   shell.env['NODE_ENV'] = 'development';
   shell.env['DEV_CLEAN'] = process.argv.includes('--clean') ? '1' : '0';
@@ -54,7 +56,7 @@ async function buildElectron(viteUrl) {
 }
 
 async function createViteServer() {
-  await buildPreload();
+  const { tsconfig: WEB_RAW_TSCONFIG } = await parse(path.resolve(process.cwd(), 'src/client/tsconfig.web.json'));
   const server = await createServer({
     configFile: false,
     root: './src/client/driver/web',
@@ -68,6 +70,9 @@ async function createViteServer() {
     define: {
       __WEB_ENV__: JSON.stringify('electron-renderer'),
     },
+    esbuild: {
+      tsconfigRaw: WEB_RAW_TSCONFIG,
+    },
   });
 
   await server.listen();
@@ -78,6 +83,7 @@ async function createViteServer() {
 
 (async () => {
   shell.exec('clear');
+  await buildPreload();
   const viteUrl = await createViteServer();
   const ELECTRON_RELATED_DIRS = ['src/server', 'src/client/driver/electron', 'src/shared'];
   let shellProcess = await buildElectron(viteUrl);
