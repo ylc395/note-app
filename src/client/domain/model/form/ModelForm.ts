55238a1fb1c19406659ba6274b3cf6561a4bd7a7
ylc395 <cyl@cyl.moe>
2022-11-05 22:26:37 +0800

    watch for server error

--- src/client/domain/model/form/ModelForm.ts
+++ src/client/domain/model/form/ModelForm.ts
@@ -2,6 +2,10 @@ import { ref, toRaw } from '@vue/reactivity';
 import { watch, watchEffect, type WatchStopHandle } from '@vue-reactivity/watch';
 import type { ZodType } from 'zod';
 import debounce from 'lodash/debounce';
+import isObject from 'lodash/isObject';
+import get from 'lodash/get';
+import set from 'lodash/set';
+import unset from 'lodash/unset';
 
 import { InvalidInputError, type Issues, getIssuesFromZodError } from 'model/Error';
 
@@ -9,10 +13,12 @@ export default abstract class ModelForm<T> {
   errors = ref<Issues>({});
   #stopWatchInitialValues: WatchStopHandle;
   #stopWatchValidate?: WatchStopHandle;
+  #errorFieldValidateStoppers = new Set<WatchStopHandle>();
+
   protected abstract readonly schema: ZodType<T>;
   readonly values = ref<T>();
 
-  constructor(private initialValues: () => T) {
+  constructor(private readonly initialValues: () => T) {
     this.#stopWatchInitialValues = watchEffect(() => {
       this.values.value = this.initialValues();
     });
@@ -34,6 +40,7 @@ export default abstract class ModelForm<T> {
       const isValid = this.#validate();
 
       if (!isValid) {
+        // when local validation is not passed, start to watch & validate
         if (!this.#stopWatchValidate) {
           this.#stopWatchValidate = watch(this.values, debounce(this.#validate, 500), { deep: true });
         }
@@ -41,7 +48,6 @@ export default abstract class ModelForm<T> {
       }
 
       // local validation passed. Waiting for remote validation
-
       try {
         await submit(toRaw(this.values.value));
         this.reset();
@@ -52,10 +58,34 @@ export default abstract class ModelForm<T> {
         }
 
         this.errors.value = e.issues;
+        this.#watchErrorFields(this.errors.value);
         return false;
       }
     };
   };
+
+  #watchErrorFields(obj: unknown, path: string[] = []) {
+    if (isObject(obj)) {
+      for (const key of Object.keys(obj)) {
+        this.#watchErrorFields(obj[key as keyof typeof obj], [...path, key]);
+      }
+    } else {
+      const wrongValue = get(this.values.value, path);
+      const errorMessage = obj;
+      this.#errorFieldValidateStoppers.add(
+        watch(
+          () => get(this.values.value, path),
+          (value) => {
+            if (wrongValue === value) {
+              set(this.errors.value, path, errorMessage);
+            } else {
+              unset(this.errors.value, path);
+            }
+          },
+        ),
+      );
+    }
+  }
 
   readonly reset = () => {
     this.errors.value = {};
@@ -65,9 +95,15 @@ export default abstract class ModelForm<T> {
       this.#stopWatchValidate();
       this.#stopWatchValidate = undefined;
     }
+
+    Array.from(this.#errorFieldValidateStoppers).forEach((stop) => stop());
+    this.#errorFieldValidateStoppers.clear();
   };
 
   readonly destroy = () => {
     this.#stopWatchInitialValues();
+    this.#stopWatchValidate?.();
+    Array.from(this.#errorFieldValidateStoppers).forEach((stop) => stop());
+    this.#errorFieldValidateStoppers.clear();
   };
 }
