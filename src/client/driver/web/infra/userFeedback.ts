b8962ba1533f6e6b64a5a21f88f10d624584b0e2
ylc395 <cyl@cyl.moe>
2023-02-08 13:28:10 +0800

    message style fix

--- src/client/driver/web/infra/userFeedback.ts
+++ src/client/driver/web/infra/userFeedback.ts
@@ -2,11 +2,16 @@ import { Modal as antdModal, message as antdMessage } from 'antd';
 
 import type UserFeedback from 'infra/UserFeedback';
 
+const getContainer = () => document.querySelector('#app') as HTMLElement;
+
+antdMessage.config({ getContainer });
+
 const modal: UserFeedback['modal'] = {
   success: (options) => {
     return new Promise((resolve) => {
       antdModal.success({
         ...options,
+        getContainer,
         autoFocusButton: null,
         okText: '好的',
         onOk: resolve,
@@ -17,7 +22,11 @@ const modal: UserFeedback['modal'] = {
 
 const message: UserFeedback['message'] = {
   success: (options) => {
-    return new Promise((resolve) => antdMessage.success(options).then(() => resolve()));
+    return new Promise((resolve) =>
+      antdMessage
+        .success({ ...options, className: options.onClick ? 'cursor-pointer' : 'cursor-default' })
+        .then(() => resolve()),
+    );
   },
 };
 
