d6999f8231b34e8448321a7826345851b1714eeb
ylc395 <cyl@cyl.moe>
2023-02-14 17:38:31 +0800

    click to sort

--- src/client/driver/web/views/Explorer/Note/Tree/Operations.tsx
+++ src/client/driver/web/views/Explorer/Note/Tree/Operations.tsx
@@ -1,11 +1,15 @@
 import { observer } from 'mobx-react-lite';
 import { container } from 'tsyringe';
-import { Tooltip, Button, Dropdown } from 'antd';
+import { Tooltip, Button, Dropdown, type TooltipProps } from 'antd';
 import { SortAscendingOutlined, FileAddOutlined, ShrinkOutlined, SettingOutlined } from '@ant-design/icons';
 
 import NoteService from 'service/NoteService';
 
 import useSort from './useSort';
+
+const tooltipProps: TooltipProps = {
+  placement: 'top',
+};
 
 export default observer(function Operations() {
   const {
@@ -17,17 +21,19 @@ export default observer(function Operations() {
   return (
     <>
       <div>
-        <Tooltip title="新建根笔记">
+        <Tooltip title="新建根笔记" {...tooltipProps}>
           <Button type="text" icon={<FileAddOutlined />} onClick={() => createNote()} />
         </Tooltip>
-        <Dropdown menu={{ items: menuOptions.get(), onClick }} placement="bottom" arrow>
-          <Button type="text" icon={<SortAscendingOutlined />} />
+        <Dropdown trigger={['click']} menu={{ items: menuOptions.get(), onClick }} placement="bottom" arrow>
+          <Tooltip title="设置排序方式" {...tooltipProps}>
+            <Button type="text" icon={<SortAscendingOutlined />} />
+          </Tooltip>
         </Dropdown>
-        <Tooltip title="折叠全部节点">
+        <Tooltip title="折叠全部节点" {...tooltipProps}>
           <Button disabled={expandedNodes.size === 0} type="text" icon={<ShrinkOutlined />} onClick={collapseAll} />
         </Tooltip>
       </div>
-      <Tooltip title="笔记树配置" className="ml-auto">
+      <Tooltip title="笔记树配置" className="ml-auto" {...tooltipProps}>
         <Button type="text" icon={<SettingOutlined />} />
       </Tooltip>
     </>
