6c1ae96f668b49bcbeca5679201c5feb858ee05d
ylc395 <cyl@cyl.moe>
2023-02-18 18:04:50 +0800

    update util

--- src/shared/utils/collection.ts
+++ src/shared/utils/collection.ts
@@ -1,6 +1,6 @@
 import type { EntityId } from 'interface/Entity';
 
-export function buildIndex<T extends { id: EntityId }>(list: T[]) {
+export function buildIndex<T extends { id: EntityId | number }>(list: T[]) {
   const index: Record<string, T> = {};
 
   for (const item of list) {
