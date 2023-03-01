a840a6a16069af35828d3e2bd02db4e1e79f3461
ylc395 <cyl@cyl.moe>
2023-03-01 13:43:46 +0800

    custom tabs

--- src/client/driver/web/views/Workbench/TabBar/index.tsx
+++ src/client/driver/web/views/Workbench/TabBar/index.tsx
@@ -1,40 +1,53 @@
 import { observer } from 'mobx-react-lite';
 import { container } from 'tsyringe';
-import { Tabs as AntdTabs, ConfigProvider, Button, Tooltip } from 'antd';
+import { Button, Tooltip } from 'antd';
 import { CloseOutlined } from '@ant-design/icons';
+import { useEffect, useRef } from 'react';
 
 import EditorService from 'service/EditorService';
 import type Tile from 'model/workbench/Tile';
 
-import TabItem from './TabItem';
-
 export default observer(function TabBar({ tileId }: { tileId: Tile['id'] }) {
   const { tileManager } = container.resolve(EditorService);
   const { switchToEditor, closeEditor, currentEditor, editors, closeAllEditors } = tileManager.get(tileId);
+  const rootRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!rootRef.current || !currentEditor) {
+      return;
+    }
+
+    const tabEl = rootRef.current.querySelector(`[data-editor-id=${currentEditor.id}]`) as HTMLElement;
+    tabEl.scrollIntoView();
+  }, [currentEditor]);
 
   if (!currentEditor) {
     return null;
   }
 
   return (
-    <ConfigProvider theme={{ components: { Tabs: { margin: 0 } } }}>
-      <AntdTabs
-        type="editable-card"
-        className="w-full bg-gray-100"
-        hideAdd
-        onChange={switchToEditor}
-        onEdit={(key) => typeof key === 'string' && closeEditor(key)}
-        items={editors.map((tab) => ({ label: tab.title, key: tab.id }))}
-        activeKey={currentEditor.id}
-        renderTabBar={(tabBarProps, DefaultTabBar) => (
-          <DefaultTabBar {...tabBarProps}>{(node) => <TabItem>{node}</TabItem>}</DefaultTabBar>
-        )}
-        tabBarExtraContent={
-          <Tooltip title="关闭全部">
-            <Button onClick={closeAllEditors} type="text" icon={<CloseOutlined />} />
-          </Tooltip>
-        }
-      />
-    </ConfigProvider>
+    <div ref={rootRef} className="flex justify-between border-0 border-b border-solid border-gray-200">
+      <div className="scrollbar-hidden flex overflow-auto">
+        {editors.map(({ id, title }) => (
+          <div
+            data-editor-id={id}
+            className={`flex cursor-pointer items-center whitespace-nowrap border-0 border-r border-solid border-gray-200 bg-gray-100 p-2 ${
+              currentEditor.id === id ? 'bg-white' : ''
+            }`}
+            key={id}
+          >
+            <span className="max-w-[200px] overflow-hidden text-ellipsis text-sm" onClick={() => switchToEditor(id)}>
+              {title}
+            </span>
+            <Button size="small" onClick={() => closeEditor(id)} type="text" icon={<CloseOutlined />} />
+          </div>
+        ))}
+      </div>
+      <div className="flex items-center">
+        <Tooltip title="关闭全部">
+          <Button onClick={closeAllEditors} type="text" icon={<CloseOutlined />} />
+        </Tooltip>
+      </div>
+    </div>
   );
 });
