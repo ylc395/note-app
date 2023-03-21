3ed0bfb33d363f3c31a007f0efad9f72f66963e4
ylc395 <cyl@cyl.moe>
2023-03-21 23:41:50 +0800

    remove invalid code

--- src/client/driver/web/infra/storage.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  set<T>(path: string, value: T) {
-    localStorage.setItem(path, JSON.stringify(value));
-  },
-
-  get<T>(path: string): NonNullable<T> | null {
-    return JSON.parse(localStorage.getItem(path) || 'null');
-  },
-};
