38cce56e8c16fa139de1b898dddbb452d9bd9fdc
ylc395 <cyl@cyl.moe>
2023-05-16 14:39:15 +0800

    pdf editor init

--- src/client/driver/web/views/Workbench/Editor/PdfEditor/index.tsx
+++ src/client/driver/web/views/Workbench/Editor/PdfEditor/index.tsx
@@ -1,7 +1,33 @@
 import { observer } from 'mobx-react-lite';
+import { useEffect, useRef } from 'react';
 
 import type PdfEditor from 'model/material/PdfEditor';
+import Core from './Core';
 
 export default observer(function PdfEditorView({ editor }: { editor: PdfEditor }) {
-  return <div>{editor.entity?.blob.byteLength}</div>;
+  const containerElRef = useRef<HTMLDivElement | null>(null);
+  const viewerElRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!editor.entity || !containerElRef.current || !viewerElRef.current) {
+      return;
+    }
+
+    const core = new Core({
+      container: containerElRef.current,
+      viewer: viewerElRef.current,
+    });
+
+    core.load(editor.entity.blob.slice(0));
+
+    return () => {
+      core.destroy();
+    };
+  }, [editor.entity]);
+
+  return (
+    <div className="absolute h-full w-full overflow-auto" ref={containerElRef}>
+      <div className="select-text" ref={viewerElRef}></div>
+    </div>
+  );
 });
--- src/client/driver/web/vite-env.d.ts
+++ src/client/driver/web/vite-env.d.ts
@@ -1,2 +1,4 @@
 declare const __ENV__: 'dev' | 'prod';
 declare const __PLATFORM__: 'electron' | 'web';
+
+declare module 'pdfjs-dist/build/pdf.worker.min.js?worker&url';
